47654952623f05282b871e8075f81b508023de00
Takuya Ogawa <46732412+ayukatawago@users.noreply.github.com>
2022-12-05 21:02:34 +0900

    Solution for Dey04

---
+++ src/Day04.kt
@@ -0,0 +1,42 @@
+fun main() {
+    fun String.toIntSetPair(): Pair<Set<Int>, Set<Int>>? {
+        val splitInput = split(',')
+        if (splitInput.size != 2) return null
+
+        val firstInput = splitInput[0].split('-')
+        val secondInput = splitInput[1].split('-')
+
+        if (firstInput.size != 2 || secondInput.size != 2) return null
+
+        val firstRange = firstInput[0].toInt()..firstInput[1].toInt()
+        val secondRange = secondInput[0].toInt()..secondInput[1].toInt()
+        return firstRange.toSet() to secondRange.toSet()
+    }
+
+    fun part1(pairList: List<Pair<Set<Int>, Set<Int>>>): Int {
+        var score = 0
+        pairList.forEach { (firstSet, secondSet) ->
+            if (secondSet + firstSet == secondSet || firstSet + secondSet == firstSet) {
+                score++
+            }
+        }
+        return score
+    }
+
+    fun part2(pairList: List<Pair<Set<Int>, Set<Int>>>): Int {
+        var score = 0
+        pairList.forEach { (firstSet, secondSet) ->
+            if ((secondSet - firstSet) != secondSet || (firstSet - secondSet) != firstSet) {
+                score++
+            }
+        }
+        return score
+    }
+
+
+    val testInput = readInput("Day04_test")
+    val testPairList: List<Pair<Set<Int>, Set<Int>>> = testInput.mapNotNull { it.toIntSetPair() }
+
+    println(part1(testPairList))
+    println(part2(testPairList))
+}
